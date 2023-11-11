5c8a5d7b43f6f3c5ad94247c44e7aa050baf7654
Luna Shrestha <luna9856025308@gmail.com>
2023-11-10 20:52:15 -0600

    Asking number and giving input

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What's your first number");
+var firstnumber = Convert.ToInt32(Console.ReadLine()); //Ask nyumber, give input
+Console.WriteLine("What's your second number");
+var secondnumber = Convert.ToInt32(Console.ReadLine());
+var result = firstnumber + secondnumber;
+//var result = 1 + 5; //Ask the result, no input
+Console.WriteLine($"Sum of numbers {firstnumber} and {secondnumber} is {result}"); //give betterv result with sentence
